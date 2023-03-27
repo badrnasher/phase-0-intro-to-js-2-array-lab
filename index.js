972f8d9679bb42bbfe3e18e7fbfc7b6e03e61fd8
Badr Nasher <badrnasher@gmail.com>
2023-03-28 00:07:09 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  const newCats = cats.slice();
+  newCats.push(name);
+
+  return newCats;
+}
+
+function prependCat(name) {
+  const newCats = [name, ...cats];
+
+  return newCats;
+}
+
+function removeLastCat() {
+  const newCats = [...cats];
+  newCats.pop();
+  return newCats;
+}
+
+function removeFirstCat() {
+  const newCats = cats.slice();
+  newCats.shift();
+  return newCats;
+}
